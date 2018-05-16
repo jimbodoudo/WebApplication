ace56aede6ea901f2d9981c56399cf8cab712065
Jimmy Bourque <jbourquearchitect@gmail.com>
2018-05-16 19:33:59 -0400

    test

--- WebApplication/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication/WebApplication1/Controllers/ValuesController.cs
@@ -31,7 +31,7 @@ namespace WebApplication1.Controllers
             List<string> items = new List<string>(_grocery.Stocks);
             items.Add(_branch.Name);
             items.Add("carottes crues");
-            items.Add("hello Bruno");
+            items.Add("hello Bruno and toto !!!!");
 
             // Call app2 from app1
             HttpClient client = new HttpClient();
